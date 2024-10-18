c14a1a3cda189cff5e55da6f64e0a159c654d779
Kevin Ruhl <kevin.ruhl92@gmail.com>
2024-10-18 16:56:22 -0300

    create houses list component that renders house card items; call house list and pagination components in root page

---
+++ src/app/components/houses-list.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+import type { House } from "../api/types.api";
+import { HouseCard } from "./house-card";
+
+export const HousesList = ({ houses }: { houses: House[] }) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const pageParam = searchParams.get("page");
+    const params = new URLSearchParams(searchParams.toString());
+
+    if (!pageParam) {
+      params.set("page", "1");
+      router.push(`?${params.toString()}`);
+    }
+  }, [searchParams, router]);
+
+  return (
+    <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+      {houses.map((house) => {
+        return <HouseCard house={house} key={house.url} />;
+      })}
+    </div>
+  );
+};
