37c62086efbca7073f957cb30a0baed6dd6e794b
Kevin Ruhl <kevin.ruhl92@gmail.com>
2024-10-18 16:46:34 -0300

    create house card component with dialog, empty state, loading state, error state, and data display

---
+++ src/app/components/house-card.tsx
@@ -0,0 +1,124 @@
+"use client";
+import { useState } from "react";
+import type { Character, House } from "../api/types.api";
+import { Button } from "./button";
+import { Dialog } from "./dialog";
+import { useGetHouseMembers } from "../api/use-get-house-members";
+
+export const HouseCard = ({ house }: { house: House }) => {
+  const [showDialog, setShowDialog] = useState(false);
+
+  const handleToggleDialog = () => {
+    setShowDialog(!showDialog);
+  };
+
+  return (
+    <div
+      key={house.url}
+      className="p-4 border border-gray-200 rounded-lg shadow-md flex flex-col justify-between"
+    >
+      <h2 className="text-xl font-bold my-4">{house.name}</h2>
+      <Button type="button" onClick={() => setShowDialog(true)}>
+        view members
+      </Button>
+      <HouseCardDialog
+        house={house}
+        onToggleDialog={handleToggleDialog}
+        showDialog={showDialog}
+      />
+    </div>
+  );
+};
+
+const HouseCardDialog = ({
+  house,
+  onToggleDialog,
+  showDialog,
+}: {
+  house: House;
+  onToggleDialog: () => void;
+  showDialog: boolean;
+}) => {
+  const { data, isLoading, error } = useGetHouseMembers({
+    urls: house.swornMembers,
+    enabled: showDialog,
+    name: house.name,
+  });
+
+  return (
+    <Dialog onClose={onToggleDialog} open={showDialog}>
+      <div className="p-6 bg-white rounded-lg relative">
+        <h2 className="text-2xl font-bold mb-4 max-w-lg">
+          {house.name} Members
+        </h2>
+        <MemberCounts data={data} />
+        <LoadingState isLoading={isLoading} />
+        <ErrorState error={error} />
+        <EmptyState isLoading={isLoading} data={data} />
+        <MemberList members={data} />
+      </div>
+    </Dialog>
+  );
+};
+
+const MemberCounts = ({ data }: { data: Character[] | undefined }) => {
+  if (!data?.length) return null;
+
+  const deadCount = data.filter((member) => member.died).length;
+  const aliveCount = data.filter((member) => !member.died).length;
+
+  return (
+    <div className="absolute flex gap-1 top-5 right-12 text-sm text-white">
+      <div className="border border-red-200 rounded-full px-2 py-1 bg-red-400">
+        Dead: {deadCount}
+      </div>
+      <div className="border border-red-200 rounded-full px-2 py-1 bg-green-400">
+        Alive: {aliveCount}
+      </div>
+    </div>
+  );
+};
+
+const LoadingState = ({ isLoading }: { isLoading: boolean }) => {
+  if (!isLoading) return null;
+  return <span className="text-gray-500">Loading...</span>;
+};
+
+const ErrorState = ({ error }: { error: Error | null }) => {
+  if (!error) return null;
+  return <span className="text-red-500">Error: {error.message}</span>;
+};
+
+const EmptyState = ({
+  isLoading,
+  data,
+}: {
+  isLoading: boolean;
+  data: Character[] | undefined;
+}) => {
+  if (isLoading || data?.length) return null;
+  return (
+    <span className="text-gray-500">This house has no sworn members 😔</span>
+  );
+};
+
+const MemberList = ({ members }: { members: Character[] | undefined }) => {
+  if (!members?.length) return null;
+
+  return (
+    <ul className="space-y-4">
+      {members.map((member) => (
+        <li key={member.name} className="flex items-center space-x-2">
+          <span className="font-medium">
+            {member.name || "missing name 🤷 "} -{" "}
+          </span>
+          <span className="text-sm text-gray-500">
+            {member.died
+              ? `Not so lucky 💀. Died ${member.died}`
+              : "Still alive ✅"}
+          </span>
+        </li>
+      ))}
+    </ul>
+  );
+};
